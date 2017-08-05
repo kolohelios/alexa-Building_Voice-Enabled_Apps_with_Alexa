35c4a97f82f5bef2c2cdc2e89bab9286d0e22bb1
Jon Edwards <jedwards@Jon-Edwards-MacBook.local>
2017-08-05 15:56:24 -0700

    add getRating, getAuthor, and exit intents

--- books/index.js
+++ books/index.js
@@ -36,7 +36,7 @@ app.intent('getBook', {
   }, function(req, res) {
     let title = req.slot('TitleOne');
     let message = '';
-    
+
     if (title) {
 
       // capture additional words in title
@@ -69,11 +69,73 @@ app.intent('getBook', {
   }
 );
 
+app.intent('getRating', {
+    slots: {},
+    utterances: [
+      'what is the {average| } rating',
+      '{what\'s|whats} the {average| } rating',
+      '{average| } rating'
+    ]
+  }, function(req, res) {
+    let message = '';
+
+    let book = req.session('book');
+    if (book) {
+      message = `There are ${book.ratings_count} user ratings. The average rating is ${book.average_rating}.`;
+    } else {
+      message = 'Please say get book, followed by the title.';
+    }
+
+    res.say(message).shouldEndSession(false);
+  }
+);
+
+app.intent('getAuthor', {
+    slots: {},
+    utterances: [
+      'who is the author',
+      'who are the authors',
+      '{whos|who\'s} the author',
+      '{whats|what\'s} the author',
+      '{author|authors}',
+      'who was {it|the book} {written|authored} by',
+      'who wrote {it|the book}'
+    ]
+  }, function(req, res) {
+    let message = '';
+
+    let book = req.session('book');
+    if (book) {
+      // concatenate author names into a response
+      book.authors.forEach(function(element) {
+        author = element.author[0];
+        message += author.name + ',';
+      });
+
+      // trim whitespace and trailing comma
+      message = message.replace(/,\s*$/, '');
+    } else {
+      message = 'Please say get book, followed by the title.';
+    }
+
+    res.say(message).shouldEndSession(false);
+  }
+);
+
+app.intent('exit', {
+  slots: {},
+  utterances: [ '{quit|exit|thanks|bye|enough}' ]
+  }, function(req, res) {
+    res.say('Goodbye.').shouldEndSession(true);
+  }
+);
+
 module.exports = app;
 
 if (CONFIGURATION === 'chatskills') {
   chatskills.launch(app);
   // console client
+  console.log(app.utterances());
   let text = ' ';
   while (text.length > 0 && text != 'quit') {
     text = readlineSync.question('> ');
